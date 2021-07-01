b141243eec21f7cb3c0d57fe584b434143a77ca1
ph_santosjr <paulo.henrique@databrain.com.br>
2021-07-01 08:42:14 -0300

    new algorithms

--- README.md
+++ README.md
@@ -8,8 +8,39 @@ Some algorithms examples and exercises using Python
 
 - Sum
 - Bubble Sort
-- Palindrome
+- Insertion Sort
+- Selection Sort
+- Palindrome [https://en.wikipedia.org/wiki/Palindrome]
+- Factorial [https://en.wikipedia.org/wiki/Factorial]
 
+In python file you will find more than one version for the same algorithm. That's to show how can we improve performance for our codes.
+
+### BIG-O Notation ####
+
+|Name | BIG O |
+|---|---|
+|Constant | O(c) |
+|Linear | O(n) |
+|Quadratic | O(n^2) |
+|Cubic | O(n^3) |
+|Exponential | O(2^n) |
+|Logartithmic | O(log(n)) |
+|Log Linear | O(nlog(n)) |
+
+
+![](https://res.cloudinary.com/practicaldev/image/fetch/s--u5FI10Fg--/c_limit%2Cf_auto%2Cfl_progressive%2Cq_auto%2Cw_880/https://thepracticaldev.s3.amazonaws.com/i/9f7ruqkkz9xl0937b1nf.png)
+
+
+#### Constant Complexity ( O(c)) ####
+
+The complexity of an algorithm is said be constant if the steps required to complete the execution of an algorithm remain constant.
+
+```sum_numbers()```
+
+#### Linear Complexity ( O(n)) ####
+
+
+#### Quadratic Complexity ( O(n^2))
 
 
 ### Sources ####
@@ -17,3 +48,7 @@ Some algorithms examples and exercises using Python
 Introdução a notação Big-O para análise de algoritmos [https://www.youtube.com/watch?v=qEAZzi15Uck]
 
 Python Basics and Algorithms Programming [https://www.amazon.com.br/gp/product/B09757C778/ref=ku_mi_rw_edp_ku]
+
+Big O Notation and Algorithm Analysis with Python Examples[https://stackabuse.com/big-o-notation-and-algorithm-analysis-with-python-examples]
+
+[https://bigoquiz.com/]
--- algorithms_examples.py
+++ algorithms_examples.py
@@ -1,3 +1,6 @@
+import timeit
+
+
 def sum_numbers_loop(n: int) -> int:
     """
     BIG-O Notation = O(n)
@@ -17,7 +20,7 @@ def sum_numbers(n: int) -> float:
 
 def bubble_sort(array: list) -> list:
     """
-    BIG-O Notation = O(n**2)
+    BIG-O Notation = O(n^2)
     """
 
     is_sorted = False
@@ -29,6 +32,36 @@ def bubble_sort(array: list) -> list:
                 swap(i, i+1, array)
                 is_sorted = False
         counter += 1
+    return array
+
+
+def insertion_sort(array: list) -> list:
+    """
+    BIG-O Notation = O(n^2)
+    """
+
+    for i in range(1, len(array)):
+        j = i
+        while j > 0 and array[j] < array[j-1]:
+            swap(j, j-1, array)
+            j -= 1
+    return array
+
+
+def selection_sort(array: list) -> list:
+    """
+    BIG-O Notation = O(n^2)
+    """
+    i = 0
+    while i < len(array):
+        min_element = i
+        j = i + 1
+        while j < len(array):
+            if array[min_element] > array[j]:
+                min_element = j
+            j += 1
+        swap(i, min_element, array)
+        i += 1
     return array
 
 
@@ -65,25 +98,100 @@ def palindrome_2(word_1: str) -> bool:
     return word_1[::-1] == word_1
 
 
+def palindrome_3(word: str) -> bool:
+    reversed_string = ""
+    for i in reversed(range(len(word))):
+        reversed_string += word[i]
+    return word == reversed_string
+
+
+def palindrome_4(word: str, i=0) -> bool:
+    """
+    BIG-O Notation = O(n^2)
+    """
+    j = len(word) - 1 - i
+    if i > j:
+        return True
+    else:
+        return word[i] == word[j] and palindrome_4(word, i + 1)
+
+
+def even_or_odd(value: int) -> bool:
+    """
+    BIG-O Notation = O(1)
+    """
+    return value % 2 == 0
+
+
+def fact(n: int) -> int:
+    product = 1
+    for i in range(n):
+        product = product * (i+1)
+    return product
+
+
+def fact2(n: int) -> int:
+    if n == 0:
+        return 1
+    else:
+        return n * fact2(n-1)
+
+
 def print_star_line(size=20):
     print(size * "*")
 
 
 if __name__ == "__main__":
     print_star_line()
+
     print("Testing sum_numbers with 10")
     print(sum_numbers(10))
     print_star_line()
+
     print("Testing sum_numbers_loop with 10")
     print(sum_numbers_loop(10))
     print_star_line()
+
     print("Testing bubble_sort with [8,5,2,9,5,6,3]")
     print(bubble_sort([8, 5, 2, 9, 5, 6, 3]))
     print_star_line()
+
+    print("Testing insertion_sort( with [8,5,2,9,5,6,3]")
+    print(insertion_sort([8, 5, 2, 9, 5, 6, 3]))
+    print_star_line()
+
+    print("Testing selection_sort( with [8,5,2,9,5,6,3]")
+    print(selection_sort([8, 5, 2, 9, 5, 6, 3]))
+    print_star_line()
+
     print("Testing palindrome_1 with 'abcd', 'dcba'")
     print(palindrome_1('abcd', 'dcba'))
     print_star_line()
+
     print("Testing palindrome_2 with 'radar'")
     print(palindrome_2('radar'))
     print_star_line()
 
+    print("Testing palindrome_3 with 'ana'")
+    print(palindrome_3('ana'))
+    print_star_line()
+
+    print("Testing palindrome_4 with 'dogmaiamgod'")
+    print(palindrome_4('dogmaiamgod'))
+    print_star_line()
+
+    print("Testing even_or_odd with 2")
+    print(even_or_odd(2))
+    print_star_line()
+
+    print("Testing even_or_odd with 3")
+    print(even_or_odd(3))
+    print_star_line()
+
+    print("Testing fact with 3")
+    print(fact(3))
+    print_star_line()
+
+    print("Testing fact2 with 3")
+    print(fact2(3))
+    print_star_line()
